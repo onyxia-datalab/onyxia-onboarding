5374354fac9d5f99ce708a13ad1b159e20cd9504
trygu <trygve.falch@gmail.com>
2025-01-29 12:00:45 +0100

    Humbly fixing the linting error

--- cmd/onboarding/onyxia-onboarding.go
+++ cmd/onboarding/onyxia-onboarding.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,7 +13,12 @@ func main() {
 	r.Use(middleware.Logger)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		if _, err := w.Write([]byte("Hello World!")); err != nil {
+			log.Printf("Failed to write response: %v", err)
+		}
 	})
-	http.ListenAndServe(":3000", r)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
